59a1b7cf92e04e19b72714a9fb36e01ea7959705
salboaie <master1!23>
2015-04-17 13:15:49 +0300

    first commit

---
+++ src/SwarmDebug.js
@@ -0,0 +1,16 @@
+/*
+    Functii de debug necesare pentru SwarmClient
+ */
+
+lprint = function(){
+    console.log.apply(console, arguments);
+}
+
+
+eprint = function(){
+    console.log.apply(console, arguments);
+}
+
+cprint = function(){
+    console.log.apply(console, arguments);
+}
